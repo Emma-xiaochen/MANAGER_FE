7e87eeb079e7dda5ba96cdfe5e5c8db8199b5373
Emma-xiaochen <736202625@qq.com>
2021-07-16 01:33:06 +0800

    feat:主页结构布局

---
+++ manager-fe/src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="basic-layout">
+      <div class="nav-side"></div>
+      <div class="content-right">
+        <div class="nav-top">
+          <div class="bread">面包屑</div>
+          <div class="user">用户</div>
+        </div>
+        <div class="wrapper">
+          <div class="main-page">
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style lang="scss">
+  .basic-layout {
+    position: relative;
+
+    .nav-side {
+      position: fixed;
+      width: 200px;
+      height: 100vh;
+      background: #001529;
+      color: #ffffff;
+      overflow-y: auto;
+      transition: width .5s;
+    }
+
+    .content-right {
+      margin-left: 200px;
+
+      .nav-top {
+        height: 50px;
+        line-height: 50px;
+        display: flex;
+        justify-content: space-between;
+        border-bottom: 1px solid #dddddd;
+        padding: 0 20px;
+      }
+
+      .wrapper {
+        background: #eef0f3;
+        padding: 20px;
+        height: calc(100vh - 50px);
+
+        .main-page {
+          background: #ffffff;
+          height: 100%;
+        }
+      }
+    }
+  }
+
+</style>
---
+++ manager-fe/src/components/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>欢迎来到主界面~{{msg}}</h1>
+  <router-link to="/login">去登录</router-link>
+</template>
+
+<script>
+  export default {
+    name: 'Welcome',
+    props: ['msg']
+  }
+</script>
+
+<style scoped>
+
+</style>
