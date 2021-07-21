69ecec5a083fc1a022dd05fa95701105400d6e6a
Emma-xiaochen <736202625@qq.com>
2021-07-21 14:59:06 +0800

    feat:架构设计-环境配置

--- manager-fe/vite.config.js
+++ manager-fe/vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: 'localhost',
+    port: 8080
+  },
   plugins: [vue()]
 })
