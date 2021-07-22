b4f6acb2fb836d81620226122d92ba12b1fd0532
Emma-xiaochen <736202625@qq.com>
2021-07-22 16:19:50 +0800

    feat:登陆交互开发

--- manager-fe/src/main.js
+++ manager-fe/src/main.js
@@ -5,10 +5,13 @@ import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css';
 import request from './utils/request';
 import storage from './utils/storage';
+import api from './api';
+import store from './store';
 
 console.log('环境变量=>', import.meta.env)
 
 const app = createApp(App);
 app.config.globalProperties.$request = request;
+app.config.globalProperties.$api = api;
 app.config.globalProperties.$storage = storage;
-app.use(router).use(ElementPlus).mount('#app')
+app.use(router).use(store).use(ElementPlus).mount('#app')
