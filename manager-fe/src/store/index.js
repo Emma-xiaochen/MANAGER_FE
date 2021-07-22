b0a0a41f72bfe015c0ed5cdfac9b0d7b6a6b34c7
Emma-xiaochen <736202625@qq.com>
2021-07-22 16:22:07 +0800

    feat:登陆交互开发

---
+++ manager-fe/src/store/index.js
@@ -0,0 +1,17 @@
+/**
+ * Vuex状态管理
+ * @author Emma
+ */
+
+import { createStore } from 'vuex'
+import mutations from './mutations'
+import storage from './../utils/storage'
+
+const state = {
+  userInfo: "" || storage.getItem("userInfo") // 获取用户信息
+}
+
+export default createStore({
+  state,
+  mutations
+})
