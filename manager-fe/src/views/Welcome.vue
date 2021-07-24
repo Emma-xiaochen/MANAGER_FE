2342547bbbe6ed77a9de00578888e75de53a4c4d
Emma-xiaochen <736202625@qq.com>
2021-07-24 23:55:37 +0800

    feat:欢迎页面实现

--- manager-fe/src/views/Welcome.vue
+++ manager-fe/src/views/Welcome.vue
@@ -1,6 +1,12 @@
 <template>
-  <h1>欢迎来到主界面~{{msg}}</h1>
-  <router-link to="/login">去登录</router-link>
+  <div class="welcome">
+    <div class="content">
+      <div class="sub-title">欢迎体验</div>
+      <div class="title">通用后台管理系统</div>
+      <div class="desc">- Vue3.0+ElementPlus+Node+Mongo打造通用后管理系统</div>
+    </div>
+    <div class="img"></div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +16,40 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss">
+  .welcome {
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    .content {
+      position: relative;
+      bottom: 40px;
+      
+      .sub-title {
+        line-height: 42px;
+        font-size: 30px;
+        color: #333333;
+      }
 
+      .title {
+        line-height: 62px;
+        font-size: 40px;
+        color: #409eff;
+      }
+
+      .desc {
+        text-align: right;
+        font-size: 14px;
+        color: #999999;
+      }
+    }
+
+    .img {
+      width: 371px;
+      height: 438px;
+      background-image: url('./../assets/images/welcome.png');
+      margin-left: 105px;
+    }
+  }
 </style>
