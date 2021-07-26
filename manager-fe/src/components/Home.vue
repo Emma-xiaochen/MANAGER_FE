6e90223e5c7b6fe79730e1675b94ad3e0b87933f
Emma-xiaochen <736202625@qq.com>
2021-07-26 14:26:40 +0800

    feat:首页菜单功能实现

--- manager-fe/src/components/Home.vue
+++ manager-fe/src/components/Home.vue
@@ -1,17 +1,44 @@
 <template>
     <div class="basic-layout">
       <div class="nav-side">
-        <!-- 系统logo部分 -->
+        <!-- 系统LOGO -->
         <div class="logo">
           <img src="./../assets/logo.png" alt="">
           <span>Manager</span>
         </div>
         <!-- 导航菜单 -->
+        <el-menu
+          default-active="2"
+          background-color="#001529"
+          text-color="#ffffff"
+          router
+          :collapse="false"
+          class="nav-menu">
+          <el-submenu index="1">
+            <template #title>
+              <i class="el-icon-setting"></i>
+              <span>系统管理</span>
+            </template>
+            <el-menu-item index="1-1">用户管理</el-menu-item>
+            <el-menu-item index="1-2">菜单管理</el-menu-item>
+          </el-submenu>
+           <el-submenu index="2">
+            <template #title>
+              <i class="el-icon-setting"></i>
+              <span>审批管理</span>
+            </template>
+            <el-menu-item index="2-1">休假申请</el-menu-item>
+            <el-menu-item index="2-2">待我审批</el-menu-item>
+          </el-submenu>
+        </el-menu>
       </div>
       <div class="content-right">
         <div class="nav-top">
-          <div class="bread">面包屑</div>
-          <div class="user">用户</div>
+          <div class="nav-left">
+            <div class="menu-fold"><i class="el-icon-s-fold"></i></div>
+            <div class="bread">面包屑</div>
+          </div>
+          <div class="user-info">用户</div>
         </div>
         <div class="wrapper">
           <div class="main-page">
@@ -40,6 +67,24 @@
       color: #ffffff;
       overflow-y: auto;
       transition: width .5s;
+
+      .logo {
+        display: flex;
+        height: 50px;
+        font-size: 18px;
+        align-items: center;
+
+        img {
+          width: 32px;
+          height: 32px;
+          margin: 0 16px;
+        }
+      }
+
+      .nav-menu {
+        height: calc(100vh - 50px);
+        border-right: none;
+      }
     }
 
     .content-right {
