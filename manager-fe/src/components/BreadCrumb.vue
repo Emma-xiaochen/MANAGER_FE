cdfd00a3e3d12a5c49bfd677ae98f60eea2290d8
Emma-xiaochen <736202625@qq.com>
2021-07-27 17:34:57 +0800

    feat:前台首页实现-面包屑实现

---
+++ manager-fe/src/components/BreadCrumb.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bread-crumb">
+    <el-breadcrumb  separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item v-for="(item, index) in breadList" :key="item.path">
+        <router-link to="/welcome" v-if="index == 0">{{ item.meta.title }}</router-link>
+        <span v-else>{{ item.meta.title }}</span>
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BreadCrumb',
+    computed: {
+      breadList() {
+        return this.$route.matched;
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
