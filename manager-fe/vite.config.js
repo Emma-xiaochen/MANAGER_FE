6083c8eb4761aacd707582a4d316708e59b92d91
Emma-xiaochen <736202625@qq.com>
2021-07-27 18:55:49 +0800

    feat:修改别名

--- manager-fe/vite.config.js
+++ manager-fe/vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve( __dirname, './src' )
+    }
+  },
+  // 全局的mixin样式问题，可以通过vite进行配置
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@import '@/assets/style/base.scss';`
+  //     }
+  //   }
+  // },
   server: {
     host: 'localhost',
     port: 8080,
