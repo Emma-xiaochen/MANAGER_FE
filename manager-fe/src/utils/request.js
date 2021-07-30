b1417323acddb2ecb25661cffdc4f4a3e13aeae6
Emma-xiaochen <736202625@qq.com>
2021-07-31 00:22:51 +0800

    feat:修复bug

--- manager-fe/src/utils/request.js
+++ manager-fe/src/utils/request.js
@@ -23,7 +23,7 @@ service.interceptors.request.use((req) => {
   const headers = req.headers;
   // const { token } = storage.getItem('userInfo');
   const { token = "" } = storage.getItem('userInfo') || {};
-  if (!headers.Authorization) headers.Authorization = 'Emma ' + token;
+  if (!headers.Authorization) headers.Authorization = 'Bearer ' + token;
   return req;
 })
 
