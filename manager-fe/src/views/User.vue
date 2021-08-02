e7b18ab4200cd1dc1f032076211df84e1d3a16c3
Emma-xiaochen <736202625@qq.com>
2021-08-02 13:37:41 +0800

    feat:用户管理列表实现

--- manager-fe/src/views/User.vue
+++ manager-fe/src/views/User.vue
@@ -23,17 +23,101 @@
       </el-form>
     </div>
     <div class="base-table">
-      <div class="action"></div>
+      <div class="action">
+        <el-button type="primary">新增</el-button>
+        <el-button type="danger">批量删除</el-button>
+      </div>
+      <el-table :data="userList">
+        <el-table-column type="selection" width="55" />
+        <el-table-column
+          v-for="item in columns"
+          :key="item.prop"
+          :prop="item.prop"
+          :label="item.label"
+          :width="item.width">
+        </el-table-column>
+        <el-table-column label="操作" width="150">
+          <template #default="scope">
+            <el-button @click="handleClick(scope.row)" size="mini">编辑</el-button>
+            <el-button type="danger" size="mini">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 
 <script>
   import { onMounted, reactive, ref } from 'vue'
-  
+
   export default {
     name: 'user',
+    setup() {
+      const user = reactive({});
+      const userList = ref([
+        {
+          "state": 1,
+          "role": "0",
+          "roleList": [
+            "60180b07b1eaed6c45fbebdb",
+            "60150cb764de99631b2c3cd3",
+            "60180b59b1eaed6c45fbebdc"
+          ],
+          "deptId": [
+            "60167059c9027b7d2c520a61",
+            "60167345c6a4417f2d27506f"
+          ],
+          "userId": 1000002,
+          "userName": "admin",
+          "userEmail": "admin@imooc.com",
+          "createTime": "2021-01-17T13:32:06.381Z",
+          "lastLoginTime": "2021-01-17T13:32:06.381Z",
+          "__v": 0,
+          "job": "前端架构师",
+          "mobile": "17611020000"
+        }
+      ]);
+      const columns = reactive([
+        {
+          label: '用户ID',
+          prop: 'userId'
+        },
+        {
+          label: '用户名称',
+          prop: 'userName'
+        },
+        {
+          label: '用户邮箱',
+          prop: 'userEmail'
+        },
+        {
+          label: '用户角色',
+          prop: 'role'
+        },
+        {
+          label: '用户状态',
+          prop: 'state'
+        },
+        {
+          label: '注册时间',
+          prop: 'createTime'
+        },
+        {
+          label: '最后登录时间',
+          prop: 'lastLoginTime'
+        }
+      ])
 
+      onMounted(() => {
+        console.log('init...');
+      }) 
+
+      return {
+        user,
+        userList,
+        columns
+      }
+    }
   }
 </script>
 
