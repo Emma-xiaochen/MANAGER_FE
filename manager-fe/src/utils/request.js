43528bb995e3278b5513d3672fb52c82401bfd22
Emma-xiaochen <736202625@qq.com>
2021-08-19 18:03:47 +0800

    feat:解决mock的bug

--- manager-fe/src/utils/request.js
+++ manager-fe/src/utils/request.js
@@ -55,14 +55,16 @@ function request (options) {
     options.params = options.data;
   } 
 
+  let isMock = config.mock;
+
   if (typeof options.mock != 'undefined') {
-    config.mock = options.mock;
+    isMock = options.mock;
   }
 
   if(config.env === 'prod') {
     service.defaults.baseURL = config.baseApi
   } else {
-    service.defaults.baseURL = config.mock ? config.mockApi : config.baseApi
+    service.defaults.baseURL = isMock ? config.mockApi : config.baseApi
   }
 
   return service(options)
